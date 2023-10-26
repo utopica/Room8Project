8ebdc946bd2c6bd91accceed3de663e9b0fe97c9
utopica <h.elifokumus@gmail.com>
2023-10-26 20:49:02 +0300

    Added Room8ProjectContext class

---
+++ Room8Project.Persistence/Contexts/Room8ProjectContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Room8Project.Domain.Common;
+using Room8Project.Domain.Entities;
+
+namespace Room8Project.Persistence.Contexts
+{
+    public class Room8ProjectContext : DbContext
+    {
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<TaxiDriver> TaxiDrivers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configurations.GetString("ConnectionStrings:PostgreSQL"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasOne(p => p.Account)
+                .WithOne(ba => ba.Owner)
+                .HasForeignKey<BankAccount>(ba => ba.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
